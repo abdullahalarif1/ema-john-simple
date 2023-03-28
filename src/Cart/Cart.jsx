62ec5a2f4406dfc1a1f7b1d5f2ff7ff444e04665
abdullahalarif1 <abdullahrabzil6@gmail.com>
2023-03-28 15:48:03 +0600

    complete cart calculation

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -7,9 +7,13 @@ const Cart = ({ cart }) => {
   // total calculation
   let totalPrice = 0;
   let totalShipping = 0;
+  let quantity = 0;
   for (const product of cart) {
-    totalPrice = totalPrice + product.price;
+    product.quantity = product.quantity || 1;
+    
+    totalPrice = totalPrice + product.price * product.quantity;
     totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
   }
   // shotkora ba % ber korar system
   const tax = (totalPrice * 7) / 100;
@@ -18,13 +22,11 @@ const Cart = ({ cart }) => {
     <>
       <h3 style={{ textAlign: "center" }}>Order Summary</h3>
       <div className="cart-info" style={{ paddingLeft: "10px" }}>
-        <h5>Cart : {cart.length}</h5>
+        <h5>Selected Items Quantity : {quantity}</h5>
         <p>Total Price: ${totalPrice}</p>
         <p>Total Shipping: ${totalShipping}</p>
         <p>Tax: ${tax}</p>
-        <p style={{ fontSize: "21px"}}>
-           Grand Total: ${grandTotal}
-        </p>
+        <p style={{ fontSize: "21px" }}>Grand Total: ${grandTotal}</p>
       </div>
     </>
   );
--- src/Shop/Shop.jsx
+++ src/Shop/Shop.jsx
@@ -14,10 +14,28 @@ const Shop = () => {
       .then((data) => setProducts(data));
   }, []);
 
+  // data outside interrogation korar jonno used useEffect()
   useEffect(() => {
-    const storedCart = getShoppingCart()
-    console.log(storedCart)
-  },[])
+    // step 2: get id
+    const storedCart = getShoppingCart();
+    // ja ja korsi shob array te dukaiya save korte hobe
+    const savedCart = [];
+    // object tai loop korte for in lagbe
+    for (const id in storedCart) {
+      // step 3: get the product from products by using id
+      const addedProduct = products.find((product) => product.id === id);
+
+      if (addedProduct) {
+        // step 4: get quantity of the product
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        // step 5: ja ja korsi shob push koira dilam [] array er vitor
+        savedCart.push(addedProduct);
+      }
+    }
+    // step 6:
+    setCart(savedCart);
+  }, [products]);
 
   // event handler and create new cart for order summary
   const addToCart = (product) => {
@@ -25,29 +43,26 @@ const Shop = () => {
     const newCart = [...cart, product];
     setCart(newCart);
     // click korle local storage e store hobe tai addToDb import korlam
-    addToDb(product.id)
+    // step : 1
+    addToDb(product.id);
   };
-
 
   // ------------------------ return -------------------------------
   return (
     <div className="shop-container">
-      
       {/*--------------- products----------------- */}
       <div className="product-container">
-        {
-        products.map((product) => (
+        {products.map((product) => (
           <Products
             product={product}
             key={product.id}
             addToCart={addToCart}
           ></Products>
-        ))
-        }
+        ))}
       </div>
 
       {/*--------------- order summary------------------ */}
-      <div className="order-summary" >
+      <div className="order-summary">
         <Cart cart={cart}></Cart>
       </div>
     </div>
