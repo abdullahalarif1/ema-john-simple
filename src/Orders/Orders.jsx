cc5bc33458a791b39b67de06819bf481a5c36767
abdullahalarif1 <abdullahrabzil6@gmail.com>
2023-04-05 18:11:07 +0600

    order-product-dilay donee

--- src/Orders/Orders.jsx
+++ src/Orders/Orders.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import Cart from '../Cart/Cart';
-import { useLoaderData } from 'react-router-dom';
+import React from "react";
+import Cart from "../Cart/Cart";
+import { useLoaderData } from "react-router-dom";
+import OrderReview from "../OrderReview/OrderReview";
 
 const Orders = () => {
-    const cart = useLoaderData();
-    console.log(cart);
-    return (
-        <div className='shop-container'>
-           <div className="product-container">
-            <h3>Product: {cart.length}</h3>
-            <div>
-                
-            </div>
-           </div>
-           <div className="order-summary">
-              <Cart cart={cart}></Cart>
-           </div>
+  const cart = useLoaderData();
+  console.log(cart);
+  return (
+    <div className="shop-container">
+      <div style={{margin:'20px auto'}}>
+        <h3>Product: {cart.length}</h3>
+        <div>
+          {cart.map((cart) => (
+            <OrderReview product={cart}></OrderReview>
+          ))}
         </div>
-    );
+        <div></div>
+      </div>
+      <div className="order-summary">
+        <Cart cart={cart}></Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
